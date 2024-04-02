aa34eca74fb56817e630ab1a1bb0cc146e17885d
samuel <ferraxi@gmail.com>
2024-04-02 11:04:27 +0200

    braket 000B

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-app.use(express.static('./desktop'))
+app.use(express.static('desktop'))
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+//app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
