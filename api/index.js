75909172e20a25e66b7bbd2ee5f3f5be27d7ba5e
samuel <ferraxi@gmail.com>
2024-04-02 11:16:28 +0200

    braket 000E

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.static('public'))
 
-app.get('/lola', (req, res) => res.send('testting lola request'))
+app.get('/lola', (req, res) => res.send('testing lola request ...'))
 
 //app.listen(3000, () => console.log("Server ready on port 3000."))
 
