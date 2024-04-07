6432a3eb97d6d62a61df392b7d9a72466bff4040
samuel <ferraxi@gmail.com>
2024-04-07 15:39:26 +0200

    rescale

--- frontend/playground/showcase_chess/public/ket/component/board.js
+++ frontend/playground/showcase_chess/public/ket/component/board.js
@@ -10,11 +10,13 @@ const content = `
     }
 
     #board{
+        width: 100%;
+        height: 100%;
         margin: auto;
     }
 </style>
 <div id="root">
-    <svg id="board" width="100%" height="100%">
+    <svg id="board">
     </svg>
 </div>
 `
@@ -32,29 +34,39 @@ export default class Board extends Element{
         return this.get('board')
     }
 
-    square(r,c){
+    cell(r,c){
         let color = (r % 2 && !(c % 2)) || (c % 2 && !(r % 2)) ? 'green' : 'white'
-        return `<rect x="${c}" y="${r}" width="1" height="1" fill="${color}"/>`
+        return `<rect class="cell" id="${c}-${r}" x="${c}" y="${r}" width="1" height="1" fill="${color}"/>`
     }
 
     show(height){
-        let board = this.get('board')
-
         if (height)
-            board.style.height = height + 'px'
-
-        let grid = [...Array(8)].map((_, r) => [...Array(8)].map((_,c) => 0 ))
+            this.board.style.height = height + 'px'
 
         this.scale = {
             x: this.board.getBoundingClientRect().width / 8,
             y : this.board.getBoundingClientRect().height / 8
         }
+  
+        this.board.innerHTML.trim() && this.rescale() ||  this.build()
 
-        board.innerHTML = `<g transform="scale(${this.scale.x},${this.scale.y})">` +  grid.map((r,i)  => r.map((c,j) => this.square(i,j))).join('') + '</g>'
+        this.control()
+    }
+
+    build(){
+        let grid = [...Array(8)].map((_, r) => [...Array(8)].map((_,c) => 0 ))
+        this.board.innerHTML = `<g id="canvas" transform="scale(${this.scale.x},${this.scale.y})">` +  grid.map((r,i)  => r.map((c,j) => this.cell(i,j))).join('') + '</g>'
+    }
+
+    rescale(){
+        let canvas = this.get('canvas')
+        canvas.setAttribute('transform', `scale(${this.scale.x},${this.scale.y})`)
+       
     }
 
     control(){
-
+        let cells = this.queryAll('.cell')
+        cells.forEach(cell => cell.onclick = () => console.log(cell.id))
     }
 
 
