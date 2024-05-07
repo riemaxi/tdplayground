47966587fbc150a6f2a3113adc155d34283752e0
samuel <ferraxi@gmail.com>
2024-05-07 19:02:03 +0200

    promo

--- braket/3d/board/public/ket/space.js
+++ braket/3d/board/public/ket/space.js
@@ -2,29 +2,69 @@ import * as Three from './lib/three.module.js'
 
 export default class Space{
     constructor(){
-        this.scene = new Three.Scene()
-        this.camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-
-        this.renderer = new Three.WebGLRenderer()
-        this.renderer.setSize( window.innerWidth, window.innerHeight )
-        document.body.appendChild( this.renderer.domElement )
-
-        const geometry = new Three.BoxGeometry( 1, 1, 1 )
-        const material = new Three.MeshBasicMaterial( { color: 0x00ff00 } )
-        this.cube = new Three.Mesh( geometry, material )
-        this.scene.add( this.cube )
-        
-        this.camera.position.z = 5
-
+        this.build()
         this.render()
+    }
+
+    build(){
+        this.buildScene()
+
+        this.buildRenderer()
+
+        this.addObjects()
+        
+        this.setup()
     }
 
     render(){
         requestAnimationFrame(() => this.render())
 
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
+        this.transform()
 
         this.renderer.render(this.scene, this.camera)
+    }
+
+    buildScene(){
+        this.scene = new Three.Scene()
+        this.camera = new Three.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 500 )
+    }
+
+    buildRenderer(){
+        this.renderer = new Three.WebGLRenderer()
+        this.renderer.setSize( window.innerWidth, window.innerHeight )
+
+        document.body.appendChild( this.renderer.domElement )
+        console.log(this.renderer.domElement.style)
+    }
+
+    setup(){
+        this.camera.position.set( 0, 100, 100 )
+        this.camera.lookAt( 0, 0, 0 )
+    }
+
+    transform(){
+        this.cube.rotation.x += 0.01
+        this.cube.rotation.y += 0.01;
+    }
+
+    addObjects(){
+        this.cube = new Three.Mesh(
+            new Three.BoxGeometry( 5, 5, 5 ),
+            new Three.MeshBasicMaterial( { color: 0x00ff00 } )
+        )
+
+        const points = [
+            new Three.Vector3( -10, 0, 0 ),
+            new Three.Vector3( 0, 10, 0 ),
+            new Three.Vector3( 10, 0, 0 )
+        ]
+
+        this.line = new Three.Line(
+            new Three.BufferGeometry().setFromPoints( points ),
+            new Three.LineBasicMaterial( { color: 0x0000ff } )
+        ) 
+
+        this.scene.add( this.cube )
+        this.scene.add( this.line )
     }
 }
